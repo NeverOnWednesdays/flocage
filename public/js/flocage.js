412462d60e20b5b0d6b740bd79a36cb5a034266f
Hugo Genesse <svieg.ashcasce@gmail.com>
2020-12-27 23:11:08 -0500

    Demontrer (#5)
    * add id carrier + fix modulerFrequence
    
    * Debut de l'algo selon la position
    
    * Multiplicateur de frequence selon la position

--- public/js/flocage.js
+++ public/js/flocage.js
@@ -61,16 +61,64 @@ fluid.defaults("flocage.composition", {
         }
     }
 });
+/*
+1. [Mode Début] Phases par défaut
 
+2. [Mode Début] Quand la position est disponible:
+  2.1 On garde la position initiale en mémoire
+  2.2 Fréquence de base = int(latitude) * 10 + longitude
+  2.3 On passe au mode modulations
+
+3. [Mode Modulations] Quand une nouvelle position est disponible:
+  3.1 On vérifie si c’est la première position en mode modulations
+    3.1.1 Si oui, on fait la différence entre la position initiale
+    3.1.2 Si non, on fait la différence entre la dernière position et la nouvelle
+
+4. On additionne les deux différences (latitude et longitude)
+
+5. On fait un ratio entre le résultat et une constante
+
+6. On multiplie le résultat avec la phase initiale
+
+7. On avance le compteur de phase
+
+*/
 var geoSuccess = function(position) {
-  window.BoutonFlocage.setPosition(position);
-  window.BoutonFlocage.modulerFrequence(position);
+
   console.log(position.coords.latitude);
   var latitude_element = document.getElementById('latitude');
   latitude_element.innerHTML = position.coords.latitude.toString();
   console.log(position.coords.longitude);
   var longitude_element = document.getElementById('longitude');
   longitude_element.innerHTML = position.coords.longitude.toString();
+
+  switch(window.BoutonFlocage.getMode()) {
+    case modes.DEBUT:
+      window.BoutonFlocage.setPositionInitiale(position);
+      frequence_base = Math.floor(Math.abs(position.coords.latitude)) * 10 + Math.abs(position.coords.longitude);
+      window.BoutonFlocage.modulerFrequence(frequence_base);
+      window.BoutonFlocage.setMode(modes.PREMIERE_MODULATION);
+      break;
+    case modes.PREMIERE_MODULATION:
+      window.BoutonFlocage.ajouterPosition(position);
+      var position_initiale = window.BoutonFlocage.getPositionInitiale();
+      var diff_longitude = Math.abs(position.coords.longitude - position_initiale.coords.longitude);
+      var diff_latitude = Math.abs(position.coords.latitude - position_initiale.coords.latitude);
+      var diff = diff_longitude + diff_latitude;
+      var ratio = diff / 0.0001;
+      window.BoutonFlocage.multiplierFrequence(ratio);
+      window.BoutonFlocage.setMode(modes.MODULATIONS);
+      break;
+    case modes.MODULATIONS:
+      var derniere_position = window.BoutonFlocage.getDernierePosition();
+      var diff_longitude = Math.abs(position.coords.longitude - derniere_position.coords.longitude);
+      var diff_latitude = Math.abs(position.coords.latitude - derniere_position.coords.latitude);
+      var diff = diff_longitude + diff_latitude;
+      var ratio = diff / 0.0001;
+      window.BoutonFlocage.multiplierFrequence(ratio);
+      break;
+  }
+
 };
 var geoError = function(error) {
   switch(error.code) {
@@ -79,13 +127,31 @@ var geoError = function(error) {
   }
 };
 
+const modes = {
+    DEBUT: 'debut',
+    PREMIERE_MODULATION: 'premier_modulation',
+    MODULATIONS: 'modulations',
+    ARRET: 'arret'
+}
+
 class BoutonFlocage {
 
     constructor() {
         this.status = "nouveau";
         this.sinewaver = null;
         this.teinte = false;
-        this.position = null;
+        this.position_initiale = null;
+        this.positions = [];
+        this.mode = modes.DEBUT;
+        this.compteurModulations = 0;
+    }
+
+    ajouterPosition(position) {
+      this.positions.push(position);
+    }
+
+    getDernierePosition() {
+      return this.positions[this.positions.length - 1];
     }
 
     basculer() {
@@ -108,37 +174,36 @@ class BoutonFlocage {
         }
     }
 
-    setPosition(position) {
-      this.position = position;
+    getMode() {
+      return this.mode;
     }
 
-    modulerFrequence(position) {
+    setPositionInitiale(position) {
+      this.position_initiale = position;
+    }
+    getPositionInitiale() {
+      return this.position_initiale;
+    }
+
+    setMode(mode) {
+      this.mode = mode;
+    }
+
+    modulerFrequence(nouvelle_frequence) {
       var source_freq_values = this.sinewaver.get('carrier.source.freq.values');
+      source_freq_values[this.compteurModulations++ % source_freq_values.length] = nouvelle_frequence;
       this.sinewaver.set("carrier.source.freq.values", source_freq_values);
     }
 
-    /*
-    moduler(event) {
-        // Desktop version
-	    if (event.clientX) {
-            // normalize position to max brightness 
-            var normalized_touch = event.clientX / screen.width;
-            if (this.teinte) {
-                fetch("http://127.0.0.1:5000/bri?luminosite=" + Math.floor(normalized_touch*255).toString());
-            }
-            this.sinewaver.set("carrier.freq", normalized_touch*800);
-        }
-        // Mobile version
-	    else if (event.touches) {
-            var normalized_touch = event.touches[0].clientX / screen.width;
-		    console.log(normalized_touch);
-            if (this.teinte) {
-                fetch("http://127.0.0.1:5000/bri?luminosite=" + Math.floor(normalized_touch*255).toString());
-            }
-            this.sinewaver.set("carrier.freq", Math.floor(normalized_touch*800));
-	    }
+    multiplierFrequence(multiplicateur) {
+      var source_freq_values = this.sinewaver.get('carrier.source.freq.values');
+      var frequence_actuelle = source_freq_values[this.compteurModulations];
+      frequence_actuelle = frequence_actuelle * multiplicateur;
+      source_freq_values[this.compteurModulations++ % source_freq_values.length] = frequence_actuelle;
+      this.sinewaver.set("carrier.source.freq.values", source_freq_values);
+
     }
- */
+
 };
 
 window.addEventListener("DOMContentLoaded", (event) => {
