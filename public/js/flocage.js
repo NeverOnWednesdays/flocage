22d921022c8d307af4154f10e0927266a99f5c3c
svieg <svieg.ashcasce@gmail.com>
2021-11-25 01:15:16 +0000

    move synths to their own file synths.js

--- public/js/flocage.js
+++ public/js/flocage.js
@@ -1,7 +1,3 @@
-
-var mod = 270;
-
-
 /*
 1. [Mode Début] Phases par défaut
 
@@ -56,79 +52,9 @@ class BoutonFlocage {
         switch (this.status) {
             case "nouveau":
               var canvas = document.getElementById('canvas');
-              var spectro = Spectrogram(canvas, {
-                audio: {
-                  enable: false
-                }
-              });
-              var AudioContext = window.AudioContext;
-              spectro.connectSource(AudioContext);
               console.log("nouveau");
-              this.barfcore = flock.synth({
-                  synthDef: {
-                    id: "carrier",
-                    ugen: "flock.ugen.filter.moog",
-                    cutoff: {
-                    ugen: "flock.ugen.sinOsc",
-                    freq: 80,
-                    phase: {
-                      id: "mod",
-                      ugen: "flock.ugen.sinOsc",
-                      freq: 34.0,
-                      mul: {
-                          ugen: "flock.ugen.sinOsc",
-                          freq: 1 / mod,
-                          mul: flock.PI
-                      },
-                      add: flock.PI
-                    },
-                    mul: 5000,
-                    add: 7000
-                    },
-                    resonance: {
-                      ugen: "flock.ugen.sinOsc",
-                      freq: 1,
-                      mul: 1.5,
-                      add: 1.5
-                    },
-                    source: {
-                      ugen: "flock.ugen.lfSaw",
-                      freq: {
-                        ugen: "flock.ugen.sequence",
-                        freq: mod / 50,
-                        loop: 1,
-                        values: [mod + 13, 221 * 5/7, mod, (mod + 20) * 3/9, 220 * 4/5, 227, mod, mod * 3],
-                        options: {
-                            interpolation: "linear"
-                        }
-                      }
-                    },
-                    mul: 0.35
-                  },
-                  addToEnvironment: false
-                });
-              this.drone = flock.synth({
-                synthDef: {
-                  id: "carrier",
-                  ugen: "flock.ugen.sinOsc",
-                  freq: mod * 1.5,
-                  phase: {
-                    id: "mod",
-                    ugen: "flock.ugen.sinOsc",
-                    freq: mod * 0.5,
-                    mul: {
-                      ugen: "flock.ugen.sinOsc",
-                      freq: 1 / (mod / 5),
-                      mul: flock.PI
-                    },
-                    add: flock.PI
-                  },
-                  mul: 0.5
-                  },
-                  addToEnvironment: false
-              });
 
-                this.synths = [this.barfcore, this.drone];
+                this.synths = [barfcore, drone];
                 this.status = "joue";
                 this.enviro = flock.init();
                 this.enviro.start();
